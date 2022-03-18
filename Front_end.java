223e9adfa46ebd2e9b76cf410b43b9654935d696
jkolib <kolibachukjohn@gmail.com>
2022-03-17 22:59:00 -0400

    completed writing props. 4, 6, 7, and 8

--- Front_end.java
+++ Front_end.java
@@ -2,15 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Front_end {
-    public ArrayList<String> nodes = new ArrayList<String>();
-    public ArrayList<String> treasures = new ArrayList<String>();
+    public ArrayList<String> nodes = new ArrayList<String>(); // START
+    public ArrayList<String> treasures = new ArrayList<String>(); // GOLD
     public int num_steps;
-    public Hashtable<String, Node> encoding = new Hashtable<String, Node>();
+    public Hashtable<String, Node> encoding = new Hashtable<String, Node>(); // START --> node START
+
+    public Hashtable<String, Integer> atom_encoding = new Hashtable<String, Integer>(); // At(START,0) --> 1
+    public int atom_num = 1;
+
+    public ArrayList<String> at_atoms = new ArrayList<String>(); // At(START,0)
+    public ArrayList<String> has_atoms = new ArrayList<String>(); // Has(GOLD,0)
 
     public Front_end() throws FileNotFoundException, IOException{
         read_inp();
-
-        
+        create_at_atoms();
+        create_has_atoms();
+        write_propositions();
     }
 
     public void read_inp() throws FileNotFoundException, IOException{
@@ -82,6 +89,96 @@ public class Front_end {
         } // while
 
         br.close();
+    }
+
+    public void create_at_atoms(){
+        for(String name : this.nodes){
+            for(int i = 0; i <= this.num_steps; i++){
+                String atom_name = "At("+name+","+i+")";
+                this.at_atoms.add(atom_name);
+
+                this.atom_encoding.put(atom_name, this.atom_num);
+                this.atom_num++;
+            }
+        }
+    }
+
+    public void create_has_atoms(){
+        for(String name : this.treasures){
+            for(int i = 0; i <= this.num_steps; i++){
+                String atom_name = "Has("+name+","+i+")";
+                this.has_atoms.add(atom_name);
+                
+                this.atom_encoding.put(atom_name, this.atom_num);
+                this.atom_num++;
+            }
+        }
+    }
+
+    public String get_time(String atom){
+        return atom.substring(atom.length()-2, atom.length()-1);
+    }
+
+    public String get_name(String atom){
+        return atom.substring(atom.indexOf("(")+1, atom.indexOf(","));
+    }
+
+    public void write_propositions() throws IOException{
+        BufferedWriter bw = new BufferedWriter(new FileWriter("fe_out.txt"));
+
+        write_prop8(bw);
+        write_prop6_7(bw);
+        write_prop4(bw);
+
+        bw.write("0\n");
+
+        bw.close();
+    }
+
+    public void write_prop8(BufferedWriter bw) throws IOException{
+        for(String atom : this.has_atoms){
+            String time = get_time(atom);
+            if(time.equals(String.valueOf(this.num_steps))){
+                bw.write(this.atom_encoding.get(atom)+"\n");
+            }
+        }
+    }
+
+    public void write_prop6_7(BufferedWriter bw) throws IOException{
+        bw.write(this.atom_encoding.get("At(START,0)")+"\n");
+
+        for(String treasure : this.treasures){
+            for(String atom : this.has_atoms){
+                String time = get_time(atom);
+                String name = get_name(atom);
+
+                if(time.equals("0") && name.equals(treasure)){
+                    bw.write("-"+this.atom_encoding.get(atom)+"\n");
+                }
+            }
+        }
+    }
+
+    public void write_prop4(BufferedWriter bw) throws IOException{
+        for(int i = 1; i <= this.num_steps; i++){
+            for(String treasure : this.treasures){
+                for(String atom : this.has_atoms){
+                    String a_name = get_name(atom);
+                    int time = Integer.parseInt(get_time(atom));
+
+                    if(a_name.equals(treasure) && time == i){
+                        String prev_atom = this.has_atoms.get(this.has_atoms.indexOf(atom)-1);
+                        bw.write("-"+this.atom_encoding.get(prev_atom)+" "+this.atom_encoding.get(atom)+"\n");
+                    }
+                }
+            }
+        }
+    }
+
+    public void write_prop3(BufferedWriter bw) throws IOException{
+        for(int i = 0; i <= this.num_steps; i++){
+
+        }
     }
 }
 
