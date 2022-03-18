7b07743cb1e6433cddb6ea90ec33fbe20c2491f7
jkolib <kolibachukjohn@gmail.com>
2022-03-18 13:30:16 -0400

    main method

--- main_prog.java
+++ main_prog.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 
 public class main_prog {
     public static void main(String[] args) throws FileNotFoundException, IOException{
+        new Front_end();
         new Davis_Putnam();
+        new Back_end();
     }
 }
